9c9dba58e95daf791628005cc63a4ca1141126f2
Manzzis <mati.manzano.exe@gmail.com>
2026-04-14 20:23:46 -0300

    ,
    ,

--- scenes/creador_de_vectores.gd
+++ scenes/creador_de_vectores.gd
@@ -55,6 +55,8 @@ func _input(event) -> void:
 		queue_redraw()
 
 
+
+
 func _on_gui_input(event: InputEvent) -> void:
 	if event.is_action_pressed("clickizq"):
 		touch_down = true
--- scenes/test_map.gd
+++ scenes/test_map.gd
@@ -11,5 +11,5 @@ func _on_creador_de_vectores_vector_created(Force: Vector2) -> void:
 	var force_limit = Force.limit_length(100)
 	$objeto.apply_impulse(Vector2.ZERO, force_limit * 2000)
 	$objeto.linear_velocity = Force * 5
-	$player.new_dir.y = force_limit.y * 5
-	$player.new_dir.x = force_limit.x * 20
+	$player.new_dir.y = -force_limit.y * 5
+	$player.new_dir.x = -force_limit.x * 20
